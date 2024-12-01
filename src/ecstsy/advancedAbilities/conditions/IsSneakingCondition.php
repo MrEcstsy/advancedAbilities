72fc6c2bbc5bb72e1e677d4cde35d17374c5d943
MrEcstsy <57694923+MrEcstsy@users.noreply.github.com>
2024-12-01 03:26:50 -0800

    updated sneaking condition
    added target checking

--- src/ecstsy/advancedAbilities/conditions/IsSneakingCondition.php
+++ src/ecstsy/advancedAbilities/conditions/IsSneakingCondition.php
@@ -2,16 +2,20 @@
 
 namespace ecstsy\advancedAbilities\conditions;
 
-use ecstsy\AdvancedEnchantments\libs\ecstsy\advancedAbilities\utils\ConditionInterface;
+use ecstsy\advancedAbilities\utils\ConditionInterface;
 use pocketmine\entity\Entity;
 use pocketmine\player\Player;
 
 class IsSneakingCondition implements ConditionInterface {
 
     public function check($attacker, $victim, array $data, string $context, array $extraData): bool {
-        if ($attacker instanceof Player) {
-            return $attacker->isSneaking();
+        $target = $data['target'] ?? null;
+        $entity = ($target === 'attacker') ? $attacker : ($target === 'victim' ? $victim : null);
+
+        if (!$entity instanceof Player) {
+            return false;
         }
+
         return false;
     }
 }
