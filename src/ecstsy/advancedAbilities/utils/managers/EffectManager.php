2215fcc8bd3081130be58fd041374c9f2d14841c
MrEcstsy <57694923+MrEcstsy@users.noreply.github.com>
2024-12-01 03:16:58 -0800

    Update EffectManager.php

--- src/ecstsy/advancedAbilities/utils/managers/EffectManager.php
+++ src/ecstsy/advancedAbilities/utils/managers/EffectManager.php
@@ -3,15 +3,16 @@
 namespace ecstsy\AdvancedEnchantments\libs\ecstsy\advancedAbilities\utils\managers;
 
 use ecstsy\AdvancedEnchantments\libs\ecstsy\advancedAbilities\effects\AddPotionEffect;
+use ecstsy\AdvancedEnchantments\libs\ecstsy\advancedAbilities\effects\StealHealthEffect;
 
 class EffectManager {
 
     private static $effectMap = [
         "add_potion" => AddPotionEffect::class,
-
+        "steal_health" => StealHealthEffect::class,
     ];
 
     public static function getEffectClass(string $effectType): ?string {
-        return self::$effectMap[strtolower($effectType)] ?? null;
+        return self::$effectMap[$effectType] ?? null;
     }
 }
