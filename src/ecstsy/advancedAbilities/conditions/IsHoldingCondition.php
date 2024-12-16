852a91512676321a4118b2833920f8b7c599da6e
MrEcstsy <57694923+MrEcstsy@users.noreply.github.com>
2024-12-16 01:28:18 -0800

    Updated IS_HOLDING condition

--- src/ecstsy/advancedAbilities/conditions/IsHoldingCondition.php
+++ src/ecstsy/advancedAbilities/conditions/IsHoldingCondition.php
@@ -11,27 +11,30 @@ class IsHoldingCondition implements ConditionInterface {
 
     public function check(Entity $attacker, ?Entity $victim, array $conditionData, string $context, array $extraData): bool
     {
-        $target = $conditionData['target'] === 'victim' ? $victim : $attacker;
 
+        $target = $conditionData['target'] === 'victim' ? $victim : $attacker;
+    
         if (!$target instanceof Player) {
             return false; 
         }
+    
 
         $itemInHand = $target->getInventory()->getItemInHand();
+    
         $requiredItem = $conditionData['value'];
-
+    
         $validItems = [
             'sword' => [ItemTypeIds::WOODEN_SWORD, ItemTypeIds::STONE_SWORD, ItemTypeIds::IRON_SWORD, ItemTypeIds::GOLDEN_SWORD, ItemTypeIds::DIAMOND_SWORD, ItemTypeIds::NETHERITE_SWORD],
             'axe' => [ItemTypeIds::WOODEN_AXE, ItemTypeIds::STONE_AXE, ItemTypeIds::IRON_AXE, ItemTypeIds::GOLDEN_AXE, ItemTypeIds::DIAMOND_AXE, ItemTypeIds::NETHERITE_AXE],
             'bow' => [ItemTypeIds::BOW]
         ];
-
+        
         if (isset($validItems[$requiredItem])) {
             if (in_array($itemInHand->getTypeId(), $validItems[$requiredItem])) {
                 return true;
             }
         }
-
+    
         return false;
     }
 }
