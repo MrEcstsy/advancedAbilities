b36d4de4c1eef73635072dfaa908262aed080d0b
MrEcstsy <57694923+MrEcstsy@users.noreply.github.com>
2024-12-17 22:24:29 -0800

    Updated TriggerHelper
    * Added proper cooldown checking
    * Added force condition mode checking

--- src/ecstsy/advancedAbilities/utils/TriggerHelper.php
+++ src/ecstsy/advancedAbilities/utils/TriggerHelper.php
@@ -2,6 +2,7 @@
 
 namespace ecstsy\advancedAbilities\utils;
 
+use ecstsy\advancedAbilities\utils\managers\CooldownManager;
 use ecstsy\advancedAbilities\utils\managers\EffectManager;
 use ecstsy\advancedAbilities\utils\registries\ConditionRegistry;
 use pocketmine\entity\Entity;
@@ -19,8 +20,6 @@ trait TriggerHelper {
         $conditionType = $condition['type'] ?? '';
         $conditionMode = strtolower($condition['condition_mode'] ?? 'allow');
 
-        var_dump("Evaluating condition: {$conditionType} for target " . $target->getName() ?: "non-player");
-
         $conditionHandler = ConditionRegistry::get($conditionType);
 
         if ($conditionHandler === null) {
@@ -29,12 +28,9 @@ trait TriggerHelper {
 
         $result = $conditionHandler->check($attacker, $victim, $condition, $context, $extraData);
 
-        var_dump("Condition '{$conditionType}' result: " . ($result ? "true" : "false"));
-
         switch ($conditionMode) {
             case 'force':
                 if ($result) {
-                    var_dump("Condition '{$conditionType}' triggered FORCE.");
                     return true; 
                 }
                 break;
@@ -69,23 +65,46 @@ trait TriggerHelper {
         return true;
     }   
 
-    public function applyEffects(array $effects, Entity $caster, ?Entity $target, string $triggerContext, array $additionalData = [], int $effectChance = 100): void {
-        foreach ($effects as $effectConfig) {
+    /**
+     * Applies an array of effects to a target entity.
+     *
+     * @param array               $effects         Array of effect configurations.
+     * @param Entity              $caster          The entity applying the effect.
+     * @param Entity|null         $target          The entity to apply the effect to.
+     * @param string              $triggerContext  The context of the effect trigger.
+     * @param array               $additionalData  Additional data to pass to the effect.
+     * @param int                 $effectChance    Chance for the effect to trigger. (Default: 100)
+     * @param string              $enchantmentId   The ID of the enchantment that triggered the effect.
+     * @param bool                $forceMode       Force mode for the effect.
+     *
+     * @return void
+     */
+    public function applyEffects(array $effects, Entity $caster, ?Entity $target, string $triggerContext, array $additionalData = [], int $effectChance = 100, string $enchantmentId = '', bool $forceMode = false): void {
+        $effectCooldown = $effects['cooldown'] ?? 0;
+
+        if (!$forceMode && $caster instanceof Player && CooldownManager::isOnCooldown($caster, $enchantmentId)) {
+            return; 
+        }
+    
+        foreach ($effects['effects'] as $effectConfig) {
             $effectType = strtolower($effectConfig['type'] ?? '');
             $effectClass = EffectManager::getEffectClass($effectType);
-
+    
             if ($effectClass && class_exists($effectClass)) {
-                if ($effectChance > 0) {
+                if (!$forceMode && $effectChance > 0) {
                     $randomRoll = mt_rand(1, 100);
                     if ($randomRoll > $effectChance) {
-                        var_dump("Effect '{$effectType}' skipped due to chance ({$effectChance}%).");
                         continue; 
                     }
                 }
-
+    
                 /** @var EffectInterface $effect */
                 $effect = new $effectClass();
                 $effect->apply($caster, $target, $effectConfig, $effectConfig, $triggerContext, $additionalData);
+    
+                if (!$forceMode && $effectCooldown > 0 && $caster instanceof Player) {
+                    CooldownManager::setCooldown($caster, $enchantmentId, $effectCooldown);
+                }
             }
         }
     }
