cb55a8ad16142c6fd25b1a7c9b108af343efb494
MrEcstsy <57694923+MrEcstsy@users.noreply.github.com>
2024-12-17 22:22:12 -0800

    Better cooldown manager

--- src/ecstsy/advancedAbilities/utils/managers/CooldownManager.php
+++ src/ecstsy/advancedAbilities/utils/managers/CooldownManager.php
@@ -9,36 +9,49 @@ class CooldownManager {
     private static $cooldowns = [];
 
     /**
-     * Check if an enchantment is on cooldown for the given entity.
+     * Checks if the enchantment is on cooldown for the given entity.
      *
-     * @param Entity $entity The entity whose cooldown is being checked
-     * @param string $enchantmentId The ID of the enchantment
-     * @param int $cooldown The cooldown time in seconds
-     * @return bool True if the enchantment is on cooldown, false otherwise
+     * @param Entity $entity
+     * @param string $enchantmentId
+     * @return int Remaining cooldown time in seconds, or 0 if no cooldown is active.
      */
-    public static function isOnCooldown(Entity $entity, string $enchantmentId, int $cooldown): bool
+    public static function getRemainingCooldown(Entity $entity, string $enchantmentId): int
     {
         $currentTime = time();
 
         if (isset(self::$cooldowns[$entity->getId()][$enchantmentId])) {
-            $lastAppliedTime = self::$cooldowns[$entity->getId()][$enchantmentId];
-
-            if (($currentTime - $lastAppliedTime) < $cooldown) {
-                return true;
+            $cooldownEnd = self::$cooldowns[$entity->getId()][$enchantmentId];
+            if ($currentTime < $cooldownEnd) {
+                $remainingTime = $cooldownEnd - $currentTime;
+                return $remainingTime;
             }
         }
 
-        return false;
+        return 0;
     }
 
     /**
-     * Set the last applied time for an enchantment for a given entity.
+     * Checks if the enchantment is on cooldown.
      *
-     * @param Entity $entity The entity whose cooldown is being updated
-     * @param string $enchantmentId The ID of the enchantment
+     * @param Entity $entity
+     * @param string $enchantmentId
+     * @return bool True if the enchantment is on cooldown, false otherwise.
      */
-    public static function setCooldown(Entity $entity, string $enchantmentId): void
+    public static function isOnCooldown(Entity $entity, string $enchantmentId): bool
     {
-        self::$cooldowns[$entity->getId()][$enchantmentId] = time();
+        return self::getRemainingCooldown($entity, $enchantmentId) > 0;
+    }
+
+    /**
+     * Sets a cooldown for the enchantment for a specific entity.
+     *
+     * @param Entity $entity
+     * @param string $enchantmentId
+     * @param int $cooldown Duration of the cooldown in seconds.
+     */
+    public static function setCooldown(Entity $entity, string $enchantmentId, int $cooldown): void
+    {
+        $cooldownEnd = time() + $cooldown;
+        self::$cooldowns[$entity->getId()][$enchantmentId] = $cooldownEnd;
     }
 }
