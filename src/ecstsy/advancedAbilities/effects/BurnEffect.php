78a9dbf87fd3c7496cc8d58c4580ed93c3a050fa
MrEcstsy <57694923+MrEcstsy@users.noreply.github.com>
2024-12-19 00:41:52 -0800

    Updated Burn effect
    * Added error logging

--- src/ecstsy/advancedAbilities/effects/BurnEffect.php
+++ src/ecstsy/advancedAbilities/effects/BurnEffect.php
@@ -6,19 +6,51 @@ use ecstsy\advancedAbilities\utils\EffectInterface;
 use ecstsy\advancedAbilities\utils\Utils;
 use pocketmine\entity\Entity;
 use pocketmine\entity\Living;
+use pocketmine\player\Player;
 
 class BurnEffect implements EffectInterface {
 
     public function apply(Entity $attacker, ?Entity $victim, array $data, array $effectData, string $context, array $extraData): void
     {
-        if (isset($effectData['seconds'])) {
-            $target = $effectData['target'] === 'victim' ? $victim : $attacker;
+        $target = $effectData['target'] === 'victim' ? $victim : $attacker;
 
-            if (!$target instanceof Living) {
-                return;
+        if (!$target instanceof Living) {
+            return;
+        }
+
+        $effectType = $effectData['type'] ?? 'unknown';
+        $enchantName = $extraData['enchant-name'];
+        $errorMessages = [];
+        
+        if (!isset($effectData['seconds'])) {
+            $errorMessages[] = "Missing 'seconds' key under effect type '{$effectType}' in enchantment '{$enchantName}'.";
+        }
+
+        if (!isset($effectData['target'])) {
+            $errorMessages[] = "Missing 'target' key under effect type '{$effectType}' in enchantment '{$enchantName}'.";
+        }
+
+        if (!empty($errorMessages)) {
+            $contextInfo = [
+                "effect" => $effectType,
+                'enchant-name' => $enchantName
+            ];
+
+            if ($attacker instanceof Player) {
+                foreach ($errorMessages as $message) {
+                    Utils::sendError($attacker, $message, $contextInfo);
+                }
             }
 
-            $target->setOnFire(Utils::parseRandomNumber($effectData['seconds']));
+            if ($victim instanceof Player) {
+                foreach ($errorMessages as $message) {
+                    Utils::sendError($victim, $message, $contextInfo);
+                }
+            }
+
+            return;
         }
+
+        $target->setOnFire(Utils::parseRandomNumber($effectData['seconds']));
     }
 }
