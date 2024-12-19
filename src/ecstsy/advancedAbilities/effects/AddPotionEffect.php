4691fbad699e224f03d0c142cae0e92fff24b20c
MrEcstsy <57694923+MrEcstsy@users.noreply.github.com>
2024-12-19 00:46:37 -0800

    Update AddPotionEffect.php

--- src/ecstsy/advancedAbilities/effects/AddPotionEffect.php
+++ src/ecstsy/advancedAbilities/effects/AddPotionEffect.php
@@ -3,10 +3,12 @@
 namespace ecstsy\advancedAbilities\effects;
 
 use ecstsy\advancedAbilities\utils\EffectInterface;
+use ecstsy\advancedAbilities\utils\Utils;
 use pocketmine\entity\effect\EffectInstance;
 use pocketmine\entity\effect\StringToEffectParser;
 use pocketmine\entity\Entity;
 use pocketmine\entity\Living;
+use pocketmine\player\Player;
 
 class AddPotionEffect implements EffectInterface {
 
@@ -17,7 +19,40 @@ class AddPotionEffect implements EffectInterface {
         if (!$target instanceof Living) {
             return; 
         }
+        
+        $effectType = $effectData['type'] ?? 'unknown';
+        $enchantName = $extraData['enchant-name'];
+        $errorMessages = [];
+        
+        if (!isset($effectData['target'])) {
+            $errorMessages[] = "Missing 'target' key under effect type '{$effectType}' in enchantment '{$enchantName}'.";
+        }
 
+        if (!isset($effectData['potion'])) {
+            $errorMessages[] = "Missing 'potion' key under effect type '{$effectType}' in enchantment '{$enchantName}'.";
+        }
+
+        if (!empty($errorMessages)) {
+            $contextInfo = [
+                "effect" => $effectType,
+                'enchant-name' => $enchantName
+            ];
+
+            if ($attacker instanceof Player) {
+                foreach ($errorMessages as $message) {
+                    Utils::sendError($attacker, $message, $contextInfo);
+                }
+            }
+
+            if ($victim instanceof Player) {
+                foreach ($errorMessages as $message) {
+                    Utils::sendError($victim, $message, $contextInfo);
+                }
+            }
+
+            return;
+        }
+        
         $potion = StringToEffectParser::getInstance()->parse($effectData['potion'] ?? '');
         
         if ($potion === null) return;
