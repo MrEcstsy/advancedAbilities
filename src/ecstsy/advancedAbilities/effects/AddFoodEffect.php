1a67bbef80641b68c49c893a2d0c5a5728888a93
MrEcstsy <57694923+MrEcstsy@users.noreply.github.com>
2024-12-19 00:47:42 -0800

    Update AddFoodEffect.php

--- src/ecstsy/advancedAbilities/effects/AddFoodEffect.php
+++ src/ecstsy/advancedAbilities/effects/AddFoodEffect.php
@@ -11,18 +11,49 @@ class AddFoodEffect implements EffectInterface {
 
     public function apply(Entity $attacker, ?Entity $victim, array $data, array $effectData, string $context, array $extraData): void
     {
-        if (isset($effectData['amount'])) {
-            $target = $effectData['target'] === 'victim' ? $victim : $attacker;
+        $target = $effectData['target'] === 'victim' ? $victim : $attacker;
 
-            if (!$target instanceof Player) {
-                return;
+        if (!$target instanceof Player) {
+            return;
+        }
+
+        $effectType = $effectData['type'] ?? 'unknown';
+        $enchantName = $extraData['enchant-name'];
+        $errorMessages = [];
+        
+        if (!isset($effectData['target'])) {
+            $errorMessages[] = "Missing 'target' key under effect type '{$effectType}' in enchantment '{$enchantName}'.";
+        }
+
+        if (!isset($effectData['amount'])) {
+            $errorMessages[] = "Missing 'amount' key under effect type '{$effectType}' in enchantment '{$enchantName}'.";
+        }
+
+        if (!empty($errorMessages)) {
+            $contextInfo = [
+                "effect" => $effectType,
+                'enchant-name' => $enchantName
+            ];
+
+            if ($attacker instanceof Player) {
+                foreach ($errorMessages as $message) {
+                    Utils::sendError($attacker, $message, $contextInfo);
+                }
             }
 
-            $amount = Utils::parseRandomNumber($effectData['amount']);
-            $newFoodLevel = $target->getHungerManager()->getFood() + $amount;
-            $newFoodLevel = max(0, min(20, $newFoodLevel));
-            
-            $target->getHungerManager()->setFood($newFoodLevel);        
+            if ($victim instanceof Player) {
+                foreach ($errorMessages as $message) {
+                    Utils::sendError($victim, $message, $contextInfo);
+                }
+            }
+
+            return;
         }
+
+        $amount = Utils::parseRandomNumber($effectData['amount']);
+        $newFoodLevel = $target->getHungerManager()->getFood() + $amount;
+        $newFoodLevel = max(0, min(20, $newFoodLevel));
+        
+        $target->getHungerManager()->setFood($newFoodLevel);  
     }
 }
