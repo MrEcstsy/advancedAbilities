ed9e8ce3103dc3c1b4101a9def1f6b4e234323c4
MrEcstsy <57694923+MrEcstsy@users.noreply.github.com>
2024-12-19 00:47:25 -0800

    Update AddHealthEffect.php

--- src/ecstsy/advancedAbilities/effects/AddHealthEffect.php
+++ src/ecstsy/advancedAbilities/effects/AddHealthEffect.php
@@ -6,19 +6,51 @@ use ecstsy\advancedAbilities\utils\EffectInterface;
 use ecstsy\advancedAbilities\utils\Utils;
 use pocketmine\entity\Entity;
 use pocketmine\entity\Living;
+use pocketmine\player\Player;
 
 class AddHealthEffect implements EffectInterface {
 
     public function apply(Entity $attacker, ?Entity $victim, array $data, array $effectData, string $context, array $extraData): void
     {
-        if (isset($effectData['amount'])) {
-            $target = $effectData['target'] === 'victim' ? $victim : $attacker;
+        $target = $effectData['target'] === 'victim' ? $victim : $attacker;
 
-            if (!$target instanceof Living) {
-                return;
+        if (!$target instanceof Living) {
+            return;
+        }
+
+        $effectType = $effectData['type'] ?? 'unknown';
+        $enchantName = $extraData['enchant-name'];
+        $errorMessages = [];
+        
+        if (!isset($effectData['target'])) {
+            $errorMessages[] = "Missing 'target' key under effect type '{$effectType}' in enchantment '{$enchantName}'.";
+        }
+
+        if (!isset($effectData['amount'])) {
+            $errorMessages[] = "Missing 'amount' key under effect type '{$effectType}' in enchantment '{$enchantName}'.";
+        }
+
+        if (!empty($errorMessages)) {
+            $contextInfo = [
+                "effect" => $effectType,
+                'enchant-name' => $enchantName
+            ];
+
+            if ($attacker instanceof Player) {
+                foreach ($errorMessages as $message) {
+                    Utils::sendError($attacker, $message, $contextInfo);
+                }
             }
 
-            $target->setHealth($target->getHealth() + Utils::parseRandomNumber($effectData['amount']));
+            if ($victim instanceof Player) {
+                foreach ($errorMessages as $message) {
+                    Utils::sendError($victim, $message, $contextInfo);
+                }
+            }
+
+            return;
         }
+
+        $target->setHealth($target->getHealth() + Utils::parseRandomNumber($effectData['amount']));
     }
 }
